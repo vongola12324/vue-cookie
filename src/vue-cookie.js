cde0ac40c212eabd296edd3761a578186ce93642
Vongola <vongola12324@coder.tw>
2018-12-14 16:06:51 +0800

    Fix typeerror

--- src/vue-cookie.js
+++ src/vue-cookie.js
@@ -2,41 +2,50 @@ import {
     isCookieEnabled, getCookie, getRawCookie, getAllCookies, setCookie, setRawCookie, removeCookie,
 } from 'tiny-cookie';
 
-const VueCookie = {
-    install: function _install(Vue) {
-        /* eslint-disable no-param-reassign */
-        Vue.prototype.$cookie = this;
-        Vue.cookie = this;
-        /* eslint-enable no-param-reassign */
-    },
-    isEnabled() {
-        return isCookieEnabled();
-    },
-    set(key, value, options = null) {
-        setCookie(key, value, options);
-    },
-    setRaw(key, value, options = null) {
-        setRawCookie(key, value, options);
-    },
-    get(key) {
-        return getCookie(key);
-    },
-    getAll() {
-        return getAllCookies();
-    },
-    getRaw(key) {
-        return getRawCookie(key);
-    },
-    remove(key, options = null) {
-        removeCookie(key, options);
-    },
-    flush() {
-        const cookieKeys = Object.keys(this.getAll());
-        const that = this;
-        cookieKeys.forEach((element) => {
-            that.remove(element);
-        });
-    },
-};
+(function() {
+    const VueCookie = {
+	install(Vue, options) {
+	    /* eslint-disable no-param-reassign */
+	    Vue.prototype.$cookie = this;
+	    Vue.cookie = this;
+	    /* eslint-enable no-param-reassign */
+	},
+	isEnabled() {
+	    return isCookieEnabled();
+	},
+	set(key, value, options = null) {
+	    setCookie(key, value, options);
+	},
+	setRaw(key, value, options = null) {
+	    setRawCookie(key, value, options);
+	},
+	get(key) {
+	    return getCookie(key);
+	},
+	getAll() {
+	    return getAllCookies();
+	},
+	getRaw(key) {
+	    return getRawCookie(key);
+	},
+	remove(key, options = null) {
+	    removeCookie(key, options);
+	},
+	flush() {
+	    const cookieKeys = Object.keys(this.getAll());
+	    const that = this;
+	    cookieKeys.forEach((element) => {
+		that.remove(element);
+	    });
+	},
+    };
+    if (typeof exports == "object") {
+	module.exports = VueCookie;
+    } else if (typeof define == "function" && define.amd) {
+	define([], function(){ return VueCookie; })
+    } else if (window.Vue) {
+	window.VueCookie = VueCookie;
+	Vue.use(VueCookie);
+    }
+})()
 
-export default VueCookie;
